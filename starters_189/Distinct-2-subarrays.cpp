4a9044465265a94248fd9bc189d5882c7758acbf
amulya77777 <hustleamulya@gmail.com>
2025-06-05 14:57:24 +0530

    done changes in 1st Question

--- starters_189/Distinct-2-subarrays.cpp
+++ starters_189/Distinct-2-subarrays.cpp
@@ -1,29 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// your code goes here
+int main(){
     int t;cin>>t;while(t--){
-       int n;
-        cin >> n;
-        vector<int> nums(n);
-        for (int i = 0; i < n; ++i) {
-            cin >> nums[i];
-        }
-    
-   int minLen = INT_MAX;
-
-    for (int left = 0; left < n; ++left) {
-        unordered_map<int, int> freq;
-        for (int right = left; right < n; ++right) {
-            freq[nums[right]]++;
-            if (freq.size() > 2) break;
-            if (freq.size() == 2) {
-                minLen = min(minLen, right - left + 1);
-                break; 
+        int n;cin>>n;
+        vector<int> A(n);
+        for(int i=0;i<n;i++) cin>>A[i];
+        int ans = INT_MAX;
+ 
+        for(int l=0;l<n;l++){
+            unordered_set<int> s;
+            for(int r=l;r<n;r++){
+                s.insert(A[r]);
+                if(s.size() > 2) break;
+                if(s.size() == 2){
+                    ans = min(ans,r-l+1);
+                    break;
+                }
             }
         }
-    }
-    cout<< (minLen == INT_MAX ? -1 : minLen)<<endl;
+        cout<< (ans == INT_MAX? -1:ans )<<"\n";
     }
 }
